a8bac077d50b542116727ead2cb33d49195ddc3d
Deepak555 <S528142@mail.nwmissouri.edu>
2016-09-24 00:30:28 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+function sum(){
+	var x,y,z;
+	x=0.0002778*document.getElementById("input").value;
+
+	document.getElementById("a").value=x;
+
+	y=0.23884*document.getElementById("input").value;
+	document.getElementById("b").value=y;
+
+	z=0.947817*document.getElementById("input").value;
+	document.getElementById("c").value=z;
+	}
